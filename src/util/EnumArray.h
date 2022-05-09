07ac9dcc5bdfb8f43e75c3e7fa524498152708f5
pritzza <jonathanzakharov@gmail.com>
2022-05-08 17:13:15 -0700

    Fixed and improved EnumArray

--- src/util/EnumArray.h
+++ src/util/EnumArray.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <array>
+#include <cassert>
+#include <iostream>
+
 template<typename ID, typename VAL>
 class EnumArray
 {
@@ -14,16 +18,19 @@ public:
         for (int i = 0; i < this->arr.size(); ++i)
         {
             const auto& it{ il.begin() };
-            arr[i] = *(it + i);
+            const IdValPair pair{ *(it + i) };
+
+            const bool IDsOrdered{ pair.first == static_cast<ID>(i) };
+
+            assert(IDsOrdered);
+
+            arr[i] = pair;
         }
     }
 
     constexpr VAL& getVal(const ID id)
     {
         const int index{ static_cast<int>(id) };
-        const bool IDsMatch{ id == this->arr[index].first };
-
-        assert(IDsMatch);
 
         return this->arr[index].second;
     }
@@ -44,3 +51,53 @@ public:
     }
 
 };
+
+/* Usage example:
+
+enum class FooID
+{
+    a,
+    b,
+    SIZE
+};
+
+struct Foo
+{
+    int x, y;
+
+    // Optional
+    const bool operator==(const Foo& other)
+    {
+        return
+            this->x == other.x &&
+            this->y == other.y;
+    }
+};
+
+int main()
+{
+    constexpr Foo FOO1{ 10, 11 };
+    constexpr Foo FOO2{ 20, 21 };
+
+    EnumArray<FooID, Foo> foos{
+        {FooID::a, FOO1}
+        {FooID::b, FOO2},
+    };
+
+    foos.getID(FOO1); // only works if operator== exists
+    std::cout << FOO1.y << " == " << foos.getVal(FooID::a).y << '\n';
+
+
+    const bool idTest1{ foos.getID(FOO1) == FooID::a };
+    std::cout << (idTest1 ? "passed" : "failed") << " : " << (int)foos.getID(FOO1) << " ==  0" << '\n';
+
+
+    const bool idTest2{ foos.getID(FOO2) == FooID::b };
+    std::cout << (idTest2 ? "passed" : "failed") << " : " << (int)foos.getID(FOO2) << " ==  1" << '\n';
+
+    const bool valTest1{ foos.getVal(FooID::a) == FOO1 };
+    std::cout << (valTest1 ? "passed" : "failed") << " : " << foos.getVal(FooID::a).x << " ==  10" << '\n';
+    const bool valTest2{ foos.getVal(FooID::b) == FOO2 };
+    std::cout << (valTest2 ? "passed" : "failed") << " : " << foos.getVal(FooID::b).x << " ==  20" << '\n';
+}
+*/
